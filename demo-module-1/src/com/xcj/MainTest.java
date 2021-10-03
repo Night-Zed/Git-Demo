d1d519595c10d541e43908ccd31f3eeca313a333
Night-Zed <1525596821@qq.com>
2021-10-03 22:29:33 +0800

    尝试进行第二次更改

--- demo-module-1/src/com/xcj/MainTest.java
+++ demo-module-1/src/com/xcj/MainTest.java
@@ -2,4 +2,7 @@ package com.xcj;
 
 public class MainTest {
     //右侧为绿色说明被提交到git中---红色则相反没有被提交github中
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
 }
